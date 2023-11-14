9d9292904f01e03c09f37623f9ca7cd940064c62
Emon526 <emonats526@gmail.com>
2023-11-15 00:02:09 +0600

    Install all scipt globally

--- customscripts.sh
+++ customscripts.sh
@@ -1,19 +1,41 @@
 #!/bin/bash
 
+# Get the current platform
+platform=$(uname -s)
+
 # Get the current directory
 cwd=$(pwd)
 
-# Search the folder for files
-for file in $(ls "$cwd");
-do
+# Function to copy and make executable
+copy_and_make_executable() {
+  sudo cp "$1" /usr/local/bin
+  sudo chmod +x /usr/local/bin/$(basename "$1")
+  echo "Copied and made executable: $(basename "$1")"
+}
 
-  # Copy the file to /usr/local/bin
-  sudo cp "$cwd/$file" /usr/local/bin
-
-  # Make the file executable
-  sudo chmod +x /usr/local/bin/"$file"
-
-  # Print the status of the file
-  echo "Copied and made executable: $file"
-
-done
+# Check the platform and perform operations
+case "$platform" in
+  Linux*)
+    echo "Detected Linux"
+    for file in $(ls "$cwd"); do
+      copy_and_make_executable "$cwd/$file"
+    done
+    ;;
+  Darwin*)
+    echo "Detected macOS"
+    for file in $(ls "$cwd"); do
+      copy_and_make_executable "$cwd/$file"
+    done
+    ;;
+  CYGWIN*|MINGW32*|MSYS*|MINGW*)
+    echo "Detected Windows"
+    for file in $(ls "$cwd"); do
+      # You can add Windows-specific operations here
+      echo "Windows operation for $file"
+    done
+    ;;
+  *)
+    echo "Unsupported platform: $platform"
+    exit 1
+    ;;
+esac
